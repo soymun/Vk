42dc827f26f17f3dbe3408d6868ede645605bc77
pkekepke@mail.ru <Rjnvehpbr1+>
2022-08-14 21:54:07 +0300

    update

--- webapp/src/components/common/Preloader/Preloader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import loader from './../../../assets/image/loader.svg'
-
-
-let Preloader = (props) => {
-     return <img src={loader} />
-}
-
-export default Preloader;
